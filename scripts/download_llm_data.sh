b6f31de0c7fb55d009f8626591126691b87fa88e
MAEDA Chikara Antoine <antoineblanot@gmail.com>
2025-10-20 15:06:03 +0900

    share llm data

---
+++ scripts/download_llm_data.sh
@@ -0,0 +1,13 @@
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+DATA_FOLDER=$DIR/../data/llm
+
+mkdir -p $DATA_FOLDER
+
+# LLM data
+docker create --name tmp-llm ghcr.io/haru-project/haru-llm:ros2-fastdds
+docker cp tmp-llm:/ros2_ws/src/haru-llm/agents $DATA_FOLDER/agents
+docker cp tmp-llm:/ros2_ws/src/haru-llm/logs $DATA_FOLDER/logs
+docker rm tmp-llm
+
+# Give permissions
+sudo chmod -R a+rw $DATA_FOLDER
