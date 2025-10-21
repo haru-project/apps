ee62346198eabbd4bc13ae4127c1463692c81009
MAEDA Chikara Antoine <antoineblanot@gmail.com>
2025-10-21 13:48:05 +0900

    update speech, llm, and reasoner images

--- scripts/download_llm_data.sh
+++ scripts/download_llm_data.sh
@@ -5,7 +5,7 @@ rm -rf $DATA_FOLDER
 mkdir -p $DATA_FOLDER
 
 # LLM data
-docker create --name tmp-llm ghcr.io/haru-project/haru-llm:ros2-fastdds
+docker create --name tmp-llm ghcr.io/haru-project/haru-llm:ros2
 docker cp tmp-llm:/ros2_ws/src/haru-llm/agents $DATA_FOLDER/agents
 docker cp tmp-llm:/ros2_ws/src/haru-llm/logs $DATA_FOLDER/logs
 docker rm tmp-llm
