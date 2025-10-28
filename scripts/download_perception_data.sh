53dcfb25180606fd03f71d86532e3486568b13f9
Chikara Maeda <antoineblanot@gmail.com>
2025-10-28 14:12:06 -0400

    update data for containers

--- scripts/download_perception_data.sh
+++ scripts/download_perception_data.sh
@@ -4,6 +4,10 @@ DATA_FOLDER=$DIR/../data/perception
 rm -rf $DATA_FOLDER
 mkdir -p $DATA_FOLDER
 
+# Kinect rules
+wget -O "99-k4a.rules" "https://robotics.upo.es/~nbousan/azure_kinect_libraries/99-k4a.rules"
+sudo mv 99-k4a.rules /etc/udev/rules.d/99-k4a.rules && sudo chmod 777 /etc/udev/rules.d/99-k4a.rules
+
 # Faces data
 docker create --name tmp-faces ghcr.io/haru-project/strawberry_ros_faces_module:latest
 docker cp tmp-faces:/home/haru/.ros/strawberry_ros_faces_module $DATA_FOLDER
