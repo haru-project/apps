c96978bd2576061d8bc36da64e7203c52ff3fec0
MAEDA Chikara Antoine <antoineblanot@gmail.com>
2025-12-05 10:47:22 +0900

    Use lifecycle images for speech and llm services

--- scripts/download_speech_data.sh
+++ scripts/download_speech_data.sh
@@ -5,7 +5,7 @@ rm -rf $DATA_FOLDER
 mkdir -p $DATA_FOLDER
 
 # Voices data
-docker create --name tmp-speech ghcr.io/haru-project/haru-speech:ros2 > /dev/null
+docker create --name tmp-speech ghcr.io/haru-project/haru-speech:feature/lifecycle > /dev/null
 docker cp tmp-speech:/ros2_ws/src/haru-speech/configs $DATA_FOLDER/configs
 docker cp tmp-speech:/ros2_ws/src/haru-speech/data/voices $DATA_FOLDER/voices
 docker rm tmp-speech > /dev/null
