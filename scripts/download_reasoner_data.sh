94a50df16513f60ed7f5f34bc6f17a57e47e8e57
Chikara Maeda <chikara.maeda@jp.honda-ri.com>
2025-12-12 10:24:49 +0900

    Download and share launch config yaml files

--- scripts/download_reasoner_data.sh
+++ scripts/download_reasoner_data.sh
@@ -7,6 +7,7 @@ mkdir -p $DATA_FOLDER
 # Reasoner tasks
 docker create --name tmp-reasoner ghcr.io/haru-project/agent_reasoner:jazzy > /dev/null
 docker cp tmp-reasoner:/ros2_ws/src/agent_reasoner/haru_agent_reasoner/examples/tasks $DATA_FOLDER/tasks
+docker cp tmp-reasoner:/ros2_ws/src/agent_reasoner/haru_agent_reasoner/params $DATA_FOLDER/configs
 docker rm tmp-reasoner > /dev/null
 
 # Give permissions
