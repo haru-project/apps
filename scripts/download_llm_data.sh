72f93417fbaa67a731c447fbd38f80c8852315e5
Chikara Maeda <chikara.maeda@jp.honda-ri.com>
2026-03-09 20:05:41 +0900

    Update LLM image

--- scripts/download_llm_data.sh
+++ scripts/download_llm_data.sh
@@ -5,7 +5,7 @@ rm -rf $DATA_FOLDER
 mkdir -p $DATA_FOLDER
 
 # LLM data
-docker create --name tmp-llm ghcr.io/haru-project/haru-llm:feature-ci > /dev/null
+docker create --name tmp-llm ghcr.io/haru-project/haru-llm:feature-eval-test > /dev/null
 docker cp tmp-llm:/opt/ros/jazzy/workspace/install/share/haru_llm_ros/configs $DATA_FOLDER/configs
 docker cp tmp-llm:/opt/ros/jazzy/workspace/install/share/haru_llm_ros/agents $DATA_FOLDER/agents
 docker rm tmp-llm > /dev/null
