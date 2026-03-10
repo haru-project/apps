26838123375cd44f4c927aa3049b60dec8c1d856
Chikara Maeda <chikara.maeda@jp.honda-ri.com>
2026-03-10 18:44:31 +0900

    Update LLM and Reasoner images

--- scripts/download_reasoner_data.sh
+++ scripts/download_reasoner_data.sh
@@ -5,7 +5,7 @@ rm -rf $DATA_FOLDER
 mkdir -p $DATA_FOLDER
 
 # Reasoner data
-docker create --name tmp-reasoner ghcr.io/haru-project/haru-agent-reasoner:feature-summarize-conversation > /dev/null
+docker create --name tmp-reasoner ghcr.io/haru-project/haru-agent-reasoner:feature-migration-haru2core-v2 > /dev/null
 docker cp tmp-reasoner:/opt/ros/jazzy/workspace/install/share/haru_agent_reasoner/examples/tasks $DATA_FOLDER/tasks
 docker cp tmp-reasoner:/opt/ros/jazzy/workspace/install/share/haru_agent_reasoner/params $DATA_FOLDER/configs
 docker rm tmp-reasoner > /dev/null
